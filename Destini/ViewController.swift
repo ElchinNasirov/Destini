3bcb6466538a98c824535b6f95d689b7300bbc98
ElchinNasirov <nasirov.elchin@gmail.com>
2022-02-10 14:02:02 -0800

    Initial Commit

---
+++ Destini/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Destini
+//
+//  Created by Elchin Nasirov on 2/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
